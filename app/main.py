fd8e6846285095cf3abcb6a6c585d2499b649cef
Victor <vparagoziber25@zubirimanteo.com>
2026-07-08 16:53:57 +0100

    permission for source

--- app/main.py
+++ app/main.py
@@ -19,7 +19,8 @@ origenes_permitidos = [
 "http://localhost:3000",  # Permite pruebas desde el ordenador
 "http://localhost:3001",
 "https://my-ebooks-zeta.vercel.app"
-# "https://www.myebooks.es",     # Aquí irá el dominio final
+"https:// myebooks.es",
+"https://www.myebooks.es"     # Aquí irá el dominio final
 ]
 
 # Se inyecta el "portero" en la aplicación
